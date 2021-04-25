5176b7c7efcf1d3a27674b18f846d8c6b9210923
Jayanthi Challa <76255327+Jayanthi-Challa@users.noreply.github.com>
2021-04-25 14:59:23 +0530

    Nnat_Sum.java
    1

---
+++ Nnat_Sum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+//Sum of n natural numbers.
+class Nnat_Sum{
+	public static void main(String args[]){
+		Scanner s1=new Scanner(System.in);
+		int n=s1.nextInt();
+		int p=n*(n+1);
+		int k=p/2;
+		System.out.println(k);
+	}
+}
