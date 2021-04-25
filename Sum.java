9bb28cb4503655cb363637cd4aa2248b6aefe9c2
Jayanthi Challa <76255327+Jayanthi-Challa@users.noreply.github.com>
2021-04-25 15:00:56 +0530

    2

---
+++ Sum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+// Sum of two numbers.
+public class Sum{
+	public static void main(String args[]){
+		Scanner s=new Scanner(System.in);
+		int num1, num2, sum;
+		num1=s.nextInt();
+		num2=s.nextInt();
+		sum = num1 + num2;
+		System.out.println(sum);
+	}
+}
